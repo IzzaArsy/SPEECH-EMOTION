ede7f024525f00e3c8d636de4f7be18b512c9ede
Izzatul Arsy <131138923+IzzaArsy@users.noreply.github.com>
2024-06-27 00:29:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def get_file_content_as_string(path):
     
 @st.cache(show_spinner=False)
 def load_model():
-    model=tf.keras.models.load_model('mymodel.h5')
+    model=tf.keras.models.load_model('model_ravdess.h5')
     
     return model
 def application():
